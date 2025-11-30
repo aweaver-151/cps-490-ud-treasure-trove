6e51756e1fc34b6b5be30e32046a790f10ea955f
Andrew Weaver <weavera12@udayton.edu>
2025-11-30 17:20:49 -0500

    feat: initial heroku setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,17 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE, OPTIONS',
@@ -26,8 +31,12 @@ userRoutes(app)
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!')
+const clientDist = path.resolve(__dirname, '../..', 'frontend', 'dist')
+
+app.use(express.static(clientDist))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientDist, 'index.html'))
 })
 
 export { app }
